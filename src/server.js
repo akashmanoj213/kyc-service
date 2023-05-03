89b47add18d3ace5c82074e5a86869f7c317120a
Akash Manoj <akashmanoj@Akashs-MacBook-Pro.local>
2023-05-03 11:35:24 +0530

    Testing out build failure

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ const app = require('./app');
 
 const port = parseInt(process.env.PORT) || 8080;
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 server.listen(port, () => console.log(`App listening on port: ${port}...`));
